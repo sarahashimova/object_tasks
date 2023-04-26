6c5f89ed8fd46647e124e41d91c00d2f50177e5a
Sara S. Hashimova <sarash@code.local>
2023-04-26 18:46:55 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+//Task1
+// const users = [
+//     {
+//     username: "David",
+//     status: "online",
+//     lastActivity: 10
+//     }, 
+//     {
+//     username: "Lucy",
+//     status: "offline",
+//     lastActivity: 22
+//     },
+//     {
+//     username: "Bob",
+//     status: "offline",
+//     lastActivity: 104
+//     }
+// ];
+
+// let onlineUsers = users.filter(
+//         element => element.status === "online"
+// );
+// console.log(onlineUsers);
+
+// let usersOnlineNames =[];
+// for (let user of onlineUsers){
+//     usersOnlineNames.push(user.username);
+// }
+// alert(`The following users are online now: ${usersOnlineNames}`);
+
+//Task2 
+// const student = {
+//     fullName: "Maxim",
+//     experienceInMonths: 12,
+//     stack: ["HTML","CSS","Javascript","React"]
+// }
+
+// let giveJobToStuden = function (student,jobName){
+//     student.job = jobName;
+// }
+// let updatedObject = giveJobToStuden(student,"front-end developer");
+// alert(`Congratulations! Student ${student.fullName} has a new job!
+// It is now ${student.job}`);
+
+// Task 3
